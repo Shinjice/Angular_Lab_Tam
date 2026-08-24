9acbdb8d9341de738c159c7e60b9733c1fdd1746
Tam Desktop <Tam@brandmannen.nl>
2026-08-24 20:27:48 +0200

    add top bar

--- Angular_Lab_Tam/src/app/features/home/home.component.ts
+++ Angular_Lab_Tam/src/app/features/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { TopNavbarComponent } from '../../shared/top-navbar/top-navbar.component';
 
 @Component({
-  imports: [],
+  imports: [TopNavbarComponent],
   selector: 'app-home',
   styleUrl: './home.component.scss',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
-}
+export class HomeComponent {}
---
+++ Angular_Lab_Tam/src/app/shared/top-navbar/top-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-top-navbar',
+  styleUrl: './top-navbar.component.scss',
+  templateUrl: './top-navbar.component.html',
+})
+export class TopNavbarComponent {}
