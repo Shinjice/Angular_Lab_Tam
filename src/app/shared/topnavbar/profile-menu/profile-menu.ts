e522148c7f96ff56ee93265cfe7dffbf46fddfd0
Tam Desktop <tdna84@gmail.com>
2026-08-30 15:03:36 +0200

    add placeholder and darktheme settings

--- src/app/shared/topnavbar/profile-menu/profile-menu.ts
+++ src/app/shared/topnavbar/profile-menu/profile-menu.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile-menu.html',
   styleUrl: './profile-menu.css',
 })
 export class ProfileMenu {
+  constructor(public authService: AuthService) {}
 
+  theme = 'light';
+
+  setTheme(theme: string) {
+    this.theme = theme;
+    document.documentElement.setAttribute('data-theme', theme);
+  }
 }
